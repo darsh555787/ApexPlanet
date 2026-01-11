0bc937f701b78f4c59ff59a09e0c613215fe03da
darsh55787 <darshanjagtap594@gmail.com>
2026-01-11 17:46:45 +0530

    TASK - 1: Setting Up the Development Environment

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-$host = "127.0.0.1"; // Changed from localhost to IP
+$host = "127.0.0.1:3307"; // This forces the connection to use port 3307
 $user = "root";
-$pass = "";         // Leave empty as per your screenshot
+$pass = "";               // Empty password as shown in your phpMyAdmin
 $dbname = "blog";
 
 // Create connection
@@ -10,8 +10,5 @@ $conn = mysqli_connect($host, $user, $pass, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
-} else {
-    // Uncomment the line below to test if it works, then delete it
-    // echo "Connected successfully!"; 
 }
 ?>
