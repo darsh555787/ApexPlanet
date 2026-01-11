96da23bf6df20135f8a1603cec40c1d910a4f0d6
darsh55787 <darshanjagtap594@gmail.com>
2026-01-11 17:36:21 +0530

    TASK - 1: Setting Up the Development Environment

--- db.php
+++ db.php
@@ -1,16 +1,14 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "";
+$pass = ""; // This must be an empty string, no spaces.
 $dbname = "blog";
 
+// Create connection
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 
+// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
-}
-
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
 }
 ?>
