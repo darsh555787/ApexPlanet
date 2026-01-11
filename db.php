4c980a4600a8f33877f89cbc03d5de3ac952b2fa
darsh55787 <darshanjagtap594@gmail.com>
2026-01-11 17:56:19 +0530

    TASK - 1: Setting Up the Development Environment

--- db.php
+++ db.php
@@ -1,22 +1,20 @@
 <?php
-// 1. Force errors to show on screen
+// Enable error reporting to see if anything goes wrong
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-echo "<h1>PHP is working!</h1>"; // If you see this, PHP is fine.
-
-$host = "127.0.0.1:3307"; 
+$host = "127.0.0.1"; 
 $user = "root";
 $pass = ""; 
-$dbname = "blog";
+$dbname = "blog"; 
+$port = 3307; // MySQL port from your XAMPP
 
-// 2. Try to connect
-$conn = mysqli_connect($host, $user, $pass, $dbname);
+// Establish the connection
+$conn = mysqli_connect($host, $user, $pass, $dbname, $port);
 
-// 3. Check connection
 if (!$conn) {
-    die("<b style='color:red'>Database Connection Failed:</b> " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 } else {
-    echo "<b style='color:green'>Success! Database is connected.</b>";
+    echo "<h1 style='color:green'>ApexPlanet Database Connected Successfully!</h1>";
 }
 ?>
